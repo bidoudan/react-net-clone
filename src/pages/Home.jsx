056cac43745700db5ff156ae71b9e11b8b4b4454
bidoudan <bidoudan.mohamed@gmail.com>
2024-01-01 16:11:03 +0100

    Styling the jumbotron Component...

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+export default function Home() {
+    return (<Jumbotron.Container>
+        <Jumbotron.Title>Home page goes here</Jumbotron.Title>
+        <Jumbotron.SubTitle>Home page goes here</Jumbotron.SubTitle>
+    </Jumbotron.Container>)
+}
